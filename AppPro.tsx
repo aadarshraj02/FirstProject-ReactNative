2084212292d6d4375320c3e777fb854571ee2980
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-05 21:51:49 +0530

    applied basic styling

--- AppPro.tsx
+++ AppPro.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 
-import { View,Text } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  useColorScheme,
+  SafeAreaView,
+} from 'react-native';
 
-
-function AppPro(){
-    return(
-        <View>
-            <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non.</Text>
-        </View>
-    )
+function AppPro(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={isDarkMode ? styles.whiteText : styles.whiteText}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non.
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  whiteText: {
+    color: '#fff',
+  },
+  darkText: {
+    color: '#000',
+  },
+});
 
 export default AppPro;
